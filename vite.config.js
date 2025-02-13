fc207f35af03a899d7e2b635681fd5b45de91b20
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-02-14 01:47:57 +0530

    bugs fixed by using vercel.json file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "./",  // 👈 Ensures assets load correctly on Vercel
   plugins: [react()],
   resolve: {
     alias: {
