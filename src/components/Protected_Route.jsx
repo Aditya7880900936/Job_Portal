af3e5c6dbf9ef04c817452c01a292fa73c6cd0f3
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-02-14 01:14:42 +0530

    bugs fixed for Auth

--- src/components/Protected_Route.jsx
+++ src/components/Protected_Route.jsx
@@ -1,35 +1,22 @@
-import { useUser, useAuth } from "@clerk/clerk-react";
-import React, { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import { useUser } from "@clerk/clerk-react";
 
-const Protected_Route = ({ children }) => {
-  const { isSignedIn, user, isLoaded } = useUser();
+const ProtectedRoute = ({ children }) => {
+  const { isSignedIn, isLoaded, user } = useUser();
   const { pathname } = useLocation();
-  const { getToken } = useAuth();
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchToken = async () => {
-      if (isSignedIn) {
-        const token = await getToken({ template: "supabase" });
-        localStorage.setItem("supabaseAccessToken", token);
-      }
-      setLoading(false);
-    };
-    fetchToken();
-  }, [isSignedIn]);
-
-  if (loading) return <div>Loading...</div>;
-
-  if (isLoaded && !isSignedIn) {
+  if (isLoaded && !isSignedIn && isSignedIn !== undefined) {
     return <Navigate to="/?sign-in=true" />;
   }
 
-  if (user && !user?.unsafeMetadata?.role && pathname !== "/onBoarding") {
-    return <Navigate to="/onBoarding" />;
-  }
+  if (
+    user !== undefined &&
+    !user?.unsafeMetadata?.role &&
+    pathname !== "/onboarding"
+  )
+    return <Navigate to="/onboarding" />;
 
   return children;
 };
 
-export default Protected_Route;
+export default ProtectedRoute;
