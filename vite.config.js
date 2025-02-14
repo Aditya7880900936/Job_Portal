2a09805b94bf2964fc37f53280905d3ee71074e3
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-02-14 13:01:17 +0530

    Vercel.json file is changed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+/* eslint-disable no-undef */
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",  // 👈 Ensures assets load correctly on Vercel
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
